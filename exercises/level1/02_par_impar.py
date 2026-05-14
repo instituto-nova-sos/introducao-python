7cd1075497142bd051d4e8a528a10763caf62673
Joelma Carneiro <jojaandrade83@gmail.com>
2026-05-14 20:13:36 -0300

    Exercicio 2 - par ou impar

--- exercises/level1/02_par_impar.py
+++ exercises/level1/02_par_impar.py
@@ -17,3 +17,8 @@
 # -----------------------------------------------
 
 # Escreva seu codigo aqui:
+numero = int(input("digite um número: "))
+if numero % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
