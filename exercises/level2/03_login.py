2d9e8f8f0d29a6afe2aa48f7a65bc5881f362271
Nicolas Reis <reisnicolas63@gmail.com>
2026-05-19 16:39:19 -0300

    feat: Forçando o envio de todos os arquivos pendentes dos Levels 1 e 2

--- exercises/level2/03_login.py
+++ exercises/level2/03_login.py
@@ -26,3 +26,25 @@
 # -----------------------------------------------
 
 # Escreva seu codigo aqui:
+
+
+
+
+
+nome_correto = 'admin'
+senha_correta = '1234'
+tentativas = int(3)
+
+while tentativas > 0:
+    nome = input('Digite o nome de usuário: ')
+    senha = input('Digite sua senha: ')
+    if nome == nome_correto and senha == senha_correta:
+        print(f'Bem vindo {nome}!')
+        break
+    else:
+        tentativas -= 1
+        if tentativas > 0:
+            print(f'Senha ou usuário incorreta! Você só tem mais {tentativas} tentativas' )
+        else:
+            print('Usuário bloqueado no dispositivo!' )
+           
